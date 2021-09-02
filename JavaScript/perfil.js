5a91f166512480682289e6c4076fc728d516d2c8
DanieLazo17 <daniel.lazo92@gmail.com>
2021-09-02 17:03:48 -0300

    inyectamos código html

--- JavaScript/perfil.js
+++ JavaScript/perfil.js
@@ -31,7 +31,7 @@ function cargarOpcionesDestino(valor) {
 function cargarMensajesDestinos(valor) {
 
     var mensajes = JSON.parse(valor);
-    var opciones = new Array();
+    var opciones = [];
 
     mensajes.forEach(element => {
         opciones.push(
